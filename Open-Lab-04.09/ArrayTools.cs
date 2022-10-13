47ac9258b136228b40e503aaa8dc27178cc96d25
einnarth <113421210+einnarth@users.noreply.github.com>
2022-10-13 12:00:30 +0200

    Update ArrayTools.cs

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Open_Lab_04._09
 {
@@ -6,7 +9,8 @@ namespace Open_Lab_04._09
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
+            string[] dist = strings.Distinct().ToArray();
+            return dist;
         }
-    }
+    } 
 }
